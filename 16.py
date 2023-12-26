e112dc66612d8d9ae1b99c02f178783a6f4c884b
Prem Thatikonda <premthatikonda@Prems-MacBook-Air.local>
2023-12-27 00:35:19 +0530

    added invoice methods

--- 16.py
+++ 16.py
@@ -2,44 +2,74 @@ from datetime import datetime, timedelta
 from time import sleep
 
 class Room:
-    def __init__(self, roomID, availability, capacity):
+    def __init__(self, roomID, capacity, hourly_rate):
         self.roomID = roomID
-        self.availability = availability
         self.capacity = capacity
-    
-    def is_available(self):
-        return self.availability
-    
-    def book_room(self, user):
-        if self.is_available():
-            print(f"Room {self.roomID} booked by {user.name}")
-            self.availability = False
+        self.hourly_rate = hourly_rate
+        self.booked_slots = {}
+
+    def is_available(self, start_time, end_time):
+        for booked_start, booked_end in self.booked_slots.values():
+            if start_time < booked_end and end_time > booked_start:
+                return False
+        return True
+
+    def book_room(self, user, start_time, end_time):
+        if self.is_available(start_time, end_time):
+            booking_id = f"{user.userID}_{len(self.booked_slots) + 1}"
+            self.booked_slots[booking_id] = (start_time, end_time)
+            print(f"Room {self.roomID} booked by {user.name} from {start_time} to {end_time}. Booking ID: {booking_id}")
+            return booking_id
         else:
-            print("Room not available")
-    
+            print("Room not available for the specified time")
+            return None
+
+    def calculate_cost(self, booking_id):
+        start_time, end_time = self.booked_slots.get(booking_id, (None, None))
+        if start_time and end_time:
+            duration = (end_time - start_time).total_seconds() / 3600  # convert seconds to hours
+            return duration * self.hourly_rate
+        else:
+            return 0
 
 class User:
     def __init__(self, name, userID):
         self.name = name
         self.userID = userID
-    
+
 class Hotel:
     def __init__(self):
         self.rooms = []
 
     def add_room(self, room):
         self.rooms.append(room)
-    
-    def book_room(self, room):
-        room.availability = "Unavailable"
+
+    def book_room(self, room, user, start_time, end_time):
+        booking_id = room.book_room(user, start_time, end_time)
+        if booking_id:
+            return booking_id
+        else:
+            return None
+
+    def generate_invoice(self, room, booking_id):
+        booking_info = room.booked_slots.get(booking_id)
+        if booking_info:
+            start_time, end_time = booking_info
+            cost = room.calculate_cost(booking_id)
+            print("----------------------------------------------------")
+            print(f"Invoice for Room {room.roomID} - Booking ID {booking_id}:")
+            print(f"Duration: {start_time} to {end_time}")
+            print(f"Total Cost: ${cost:.2f}")
+            print("----------------------------------------------------")
+        else:
+            print("Error generating invoice. Booking ID not found.")
 
     def check_availability(self):
-        available_rooms = [room for room in self.rooms if room.availability=="Available"]
         print("Fetching available rooms...")
         sleep(1.5)
         print("Available rooms:")
-        for room in available_rooms:
-            print(f"Room {room.roomID}, Capacity: {room.capacity}")
+        for room in self.rooms:
+            print(f"Room {room.roomID}, Capacity: {room.capacity}, Hourly Rate: ${room.hourly_rate}")
 
     def find_room(self, roomID):
         for room in self.rooms:
@@ -47,13 +77,12 @@ class Hotel:
                 return room
         return None
 
-
 def main():
-    room1 = Room(101, "Available",30)
-    room2 = Room(102, "Available",25)
-    room3 = Room(103, "Available",25)
-    room4 = Room(104, "Available",30)
-    room5 = Room(105, "Available",50)
+    room1 = Room(101, 30, 10.0)
+    room2 = Room(102, 25, 8.0)
+    room3 = Room(103, 25, 12.0)
+    room4 = Room(104, 30, 15.0)
+    room5 = Room(105, 50, 20.0)
 
     hotel = Hotel()
     hotel.add_room(room1)
@@ -70,7 +99,8 @@ def main():
     while True:
         print("\n1)Check available rooms")
         print("2)Book room")
-        print("3)Exit")
+        print("3)Generate Invoice")
+        print("4)Exit")
         cmd = input("\nPlease enter your command (or 'q' to quit): ")
         if cmd == '1':
             hotel.check_availability()
@@ -80,17 +110,32 @@ def main():
             rid = int(input())
             roomToBook = hotel.find_room(rid)
             if roomToBook is not None:
+                print("Enter booking start time (YYYY-MM-DD HH:MM): ", end='')
+                start_time = datetime.strptime(input(), "%Y-%m-%d %H:%M")
+                print("Enter booking end time (YYYY-MM-DD HH:MM): ", end='')
+                end_time = datetime.strptime(input(), "%Y-%m-%d %H:%M")
                 print("Booking room...")
                 sleep(1)
-                roomToBook.book_room(user1)
-                hotel.book_room(roomToBook)
+                booking_id = hotel.book_room(roomToBook, user1, start_time, end_time)
+                if booking_id:
+                    print(f"Room booked successfully. Booking ID: {booking_id}")
             else:
                 print("Room unavailable")
 
         elif cmd == '3':
+            print("\nEnter the room number for which you want to generate an invoice: ", end='')
+            rid = int(input())
+            roomToInvoice = hotel.find_room(rid)
+            if roomToInvoice is not None:
+                print("Enter booking ID: ", end='')
+                booking_id = input()
+                hotel.generate_invoice(roomToInvoice, booking_id)
+            else:
+                print("Room not found")
+
+        elif cmd == '4':
             print("Exiting. Thank you!")
             break
 
-
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
